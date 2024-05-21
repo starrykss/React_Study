75cbd920655a558df1d64b2fdef19d3052ea7713
starrykss <kss34891@gmail.com>
2024-05-21 19:51:40 +0900

    공부 내용 추가 (Udemy)

---
+++ Study/Udemy React Lectures/01. React - The Complete Guide 2024 (incl. React Router & Redux)/Section 08/01-starting-project/src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer.jsx";
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting Tough" targetTime={10} />
+        <TimerChallenge title="Pros Only" targetTime={15} />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
