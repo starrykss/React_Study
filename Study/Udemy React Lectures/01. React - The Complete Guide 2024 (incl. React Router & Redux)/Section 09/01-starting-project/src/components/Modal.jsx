f9c1b023b8c832ac7c86228e246f3c10ff4c2906
starrykss <kss34891@gmail.com>
2024-06-02 21:23:50 +0900

    공부 내용 추가 (Udemy)

---
+++ Study/Udemy React Lectures/01. React - The Complete Guide 2024 (incl. React Router & Redux)/Section 09/01-starting-project/src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+import Button from "./Button";
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        // (참고) showModal은 <dialog> 요소의 내장 메서드
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
+});
+
+export default Modal;
