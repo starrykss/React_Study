317b865c956dd2d187c880637d5ab888a8317983
starrykss <kss34891@gmail.com>
2024-06-25 17:05:05 +0900

    공부 내용 추가

---
+++ Study/Udemy React Lectures/01. React - The Complete Guide 2024 (incl. React Router & Redux)/Section 12/01-starting-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+import Copyright from "./components/Copyright";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+      <Copyright />
+    </>
+  );
+}
+
+export default App;
---
+++ Study/Udemy React Lectures/01. React - The Complete Guide 2024 (incl. React Router & Redux)/Section 12/01-starting-project/src/components/QuestionTimer.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    console.log("SETTING TIMEOUT");
+    const timer = setTimeout(onTimeout, timeout);
+
+    // Clean Up
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    console.log("SETTING INTERVAL");
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    // Clean Up
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
+}
